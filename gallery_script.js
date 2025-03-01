bd6efcb22c12ae0feacd7af13ace32cf0cffa420
RickOnBrawl <rikki.pakalapati18@gmail.com>
2025-03-01 11:30:16 -0800

    Added gallery script

---
+++ gallery_script.js
@@ -0,0 +1,6 @@
+const imageGallery = [
+    "https://naavik.co/wp-content/uploads/2023/10/brawl-stars.jpg",
+    "https://images.squarespace-cdn.com/content/v1/58af450eb3db2b0582612f1d/1707662368729-PA2K3W3Y2IBE3YV8TDKM/all-loading-screens-coming-in-this-update-v0-m7si1wq0yh5c1.jpeg?format=1000w",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo52HnPiUPXW9aI1yXmsx6uxPsYPhJTvesMQ&s"
+
+]
